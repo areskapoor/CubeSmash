6d332d02c4ed18cf60d3fdd7c088679de34178c7
Yeebola <35385179+Yeebola@users.noreply.github.com>
2019-11-14 11:46:03 -0800

    ddddd

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,54 +1,62 @@
-
-
-let b;
-let anotherBall;
-let jffsdff;
-
-let jdflsf;
-let fjdlkasklsa;
-let fklsdfjfwkrfwdfasfdffsdfasfasdfewfrewfsdfsdfs;
+let velocityX=0;
+let velocityY=0;
 
 function setup() {
-  createCanvas(600, 600);
-
-  b = new beaches(0,100,"red")
-  anotherBall = new beaches (200,20,"green")
-  jffsdff = new beaches (300,20,"black")
+  createCanvas(400, 400);
 }
 
-function draw(){
-	background(220,100,200);
-  text("ari",30,30);
-  b.drawBall();
-  b.moveBall();
-  anotherBall.drawBall();
+function draw() {
+ print("velocityX =" + velocityX);
+  print("velocityY =" + velocityY);
+  b = new player (200,200,0);
+  b.drawme();
+  b.moveme();
 
 }
 
+function keyPressed(){
 
-class beaches {
-  constructor(x,y,color) {
-    this.x = x
-    this.y = y
-    this.color = color
+  if (keyCode === 68){
+    if (velocityX < 5){
+    velocityX += 0.5;
   }
-  drawBall(){
-    stroke(0)
-    fill(this.color)
-    ellipse(this.x,this.y,10,10)
+  }  else {
+    if (velocityX > 0){
+    velocityX -= 1
+}}
+
+
+  if (keyCode === 65){
+    if (velocityX > -5){
+    velocityX -= 0.5;
   }
-  moveBall(){
-    this.x = this.x+2
-    this.y = this.y+.5
-  }
+  } else {
+    if (velocityX < 0){
+    velocityX += 1
+}}
+
+
+//   if(b.x <= 40){
+//   if (keycode === 32){
+//
+//   }
+// }
 
 }
-class sand {
 
-  constructor(x,y,color) {
-  this.x = x
-  this.y = y
-  this.color = color
+
+class player {
+  constructor(x,y,speed){
+   this.x = x;
+    this.y = y;
+    this.speed = speed;
   }
+  drawme(){
+    rect(this.x,this.y,50,50,20,20);
+  }
+  moveme(){
+  this.y += velocityY;
+  this.X += velocityX;
 
+  }
 }
