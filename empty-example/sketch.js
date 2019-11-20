06c60b7e670fd25f8989510a3de1f1bd8b7d372c
Yeebola <35385179+Yeebola@users.noreply.github.com>
2019-11-20 15:24:02 -0800

    Update sketch.js

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,117 +1,75 @@
+let VelocityX = 0;
+let VelocityY = 0;
+let gravity = 5;
+let friction = 0.9;
+let p1;
+let p2;
+
 
 function setup() {
-  createCanvas(400, 400);
-  b = new player (200,200,0,0);
-
+  createCanvas(1000, 1000);
+  p1 = new player(200,200,30,30,3,false,false)
+  line(10,800,900,800);
 }
 
 function draw() {
- print("velocityX =" + b.velocityX);
-  print("velocityY =" + b.velocityY);
-  b.moveme();
-  b.drawme();
-
-  // if (keyIsPressed){
-  //   if (keyCode === 68){
-  //     if (velocityX < 5){
-  //     velocityX += 0.5;
-  //   }
-  // }}  else {
-  //     if (velocityX > 0){
-  //     velocityX -= 1
-  //   }}
-  //
-  // if (keyIsPressed){
-  //   if (keyCode === 65){
-  //     if (velocityX > -5){
-  //     velocityX -= 0.5;
-  //   }
-  // }} else {
-  //     if (velocityX < 0){
-  //     velocityX += 1
-  //   }}
-
-
-    //   if(b.x <= 40){
-    //   if (keycode === 32){
-    //
-    //   }
-    // }
-
-
-  }
+  p1.drawMe();
+  p1.moveMe();
+}
 
 
 
-// function keyPressed(){
-//
-//   if (keyCode === 68){
-//     if (velocityX < 5){
-//     velocityX += 0.5;
-//   }
-//   }  else {
-//     if (velocityX > 0){
-//     velocityX -= 1
-// }}
-//
-//
-//   if (keyCode === 65){
-//     if (velocityX > -5){
-//     velocityX -= 0.5;
-//   }
-//   } else {
-//     if (velocityX < 0){
-//     velocityX += 1
-// }}
-//
-//
-// //   if(b.x <= 40){
-// //   if (keycode === 32){
-// //
-// //   }
-// // }
-//
-// }
+
+
 
 
 class player {
-  constructor(x,y,velocityX,velocityY){
-   this.x = x;
+  constructor(x,y,w,h,speed,jumping,grounded){
+    this.x = x;
     this.y = y;
-    this.velocityX=velocityX;
-    this.velocityY=velocityY;
-
+    this.w = w;
+    this.h = h;
+    this.speed = speed;
+    this.jumping = jumping;
+    this.grounded = grounded;
   }
-  drawme(){
-    rect(this.x,this.y,50,50,20,20);
-  }
-  moveme(){
-  this.y += this.velocityY;
-  this.X += this.velocityX;
-  if (keyIsPressed === true){
-    if (keyCode === 68){
-        if (this.velocityX < 5){
-        this.velocityX += 0.5;
-      }
-    }}
-        if (this.velocityX > 0){
-        this.velocityX -= 1
-      }
 
-    if (keyIsPressed === true){
-      if (keyCode === 65){
-        if (this.velocityX > -5){
-        this.velocityX -= 0.5;
-      }
-    }}
-    if (keyIsPressed === false){
-      if (this.velocityX > 0){
-      this.velocityX -= 1
+
+
+  drawMe(){
+    rect(this.x,this.y,this.w,this.h,10);
+  }
+  moveMe(){
+    if (keyIsPressed === true && keyCode === 32) {
+    // up arrow or space
+    if (this.jumping === false && this.grounded === false) {
+        this.jumping = true;
+        this.grounded = false;
+        this.VelocityY = -player.speed * 2.5;//how high to jump
     }
-        if (this.velocityX < 0){
-        this.velocityX += 1
-      }
+}
+if (keyIsPressed === true && keyCode === 68) {
+    // right arrow
+    if (this.VelocityX < this.speed) {
+        this.VelocityX++;
+    }
+}
+if (keyIsPressed === true && keyCode === 65) {
+    // left arrow
+    if (this.VelocityX > -this.speed) {
+        this.VelocityX--;
+    }
+}
+  if(this.grounded === true){
+        this.VelocityY = 0;
+   }
+   this.VelocityX *= friction;
+   this.VeloctityY += gravity;
+   this.x += this.VelocityX;
+   this.y += this.VelocityY;
+   if(this.y >= 799){
+     this.grounded = true;
+   }
 
-  }
   }
 }
